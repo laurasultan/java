134eb61e8d5484e92e646a603682ab2bd76b1406
laura <105574779+laurasultan@users.noreply.github.com>
2023-04-12 12:46:18 -0300

    I = FΔt

---
+++ fisica/Impulso.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Impulso{
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        double i, f, Δt;
+        System.out.println("Digite a força:\t");
+        f = teclado.nextDouble();
+        
+        System.out.println("Digite o intervalo de tempo:\t");
+        Δt = teclado.nextDouble();
+        
+        i = f * Δt;
+        
+        System.out.printf("F = %f%n", i);
+    }
+}
