6f45f65837787e0cb68d35ca3d232f4374573b22
laura <105574779+laurasultan@users.noreply.github.com>
2023-04-12 13:02:43 -0300

    F = kx

---
+++ fisica/Elastica.java
@@ -0,0 +1,18 @@
+// Lei de Hooke
+
+import java.util.Scanner;
+public class Elastica{
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        double f, k, x;
+        System.out.println("Digite a constante elástica da mola:\t");
+        k = teclado.nextDouble();
+        
+        System.out.println("Digite deformação da mola:\t");
+        x = teclado.nextDouble();
+        
+        f = k * x;
+        
+        System.out.printf("F = %f%n", f);
+    }
+}
