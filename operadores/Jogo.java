2b928ec19d50359412327f3029f8b1cfdd8b67e2
laura <105574779+laurasultan@users.noreply.github.com>
2023-04-25 16:51:05 -0300

    Jogo

---
+++ operadores/Jogo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Jogo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int v, j1, j2;
+        // System.out.println("Escolha par ou ímpar:\t");
+        System.out.println("Escolha entre par (0) ou ímpar (1):\t");
+        v = scanner.nextInt();
+        System.out.println("Digite um número (jogador 1):\t");
+        j1 = scanner.nextInt();
+        System.out.println("Digite um número (jogador 2):\t");
+        j2 = scanner.nextInt();
+        
+        if (v == 0 && (j1 + j2) % 2 != 0) {
+            System.out.println("Jogador 2 venceu");
+        } else if (v == 1 && (j1 + j2) % 2 == 0) {
+            System.out.println("Jogador 1 venceu");
+        } else {
+            System.out.println("Jogador 2 venceu");
+        }
+    }
+}
