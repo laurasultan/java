d20735fe3a5e582345b599b5396736b386107221
laura <105574779+laurasultan@users.noreply.github.com>
2023-04-25 15:51:44 -0300

    Média

--- operadores/Media.java
+++ operadores/Media.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Media {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float n1, n2, n3, n4, media;
+        float n1, n2, n3, n4, media, exame, fim;
         System.out.println("Digite suas notas:\t");
          n1 = scanner.nextFloat();
          n2 = scanner.nextFloat();
@@ -18,9 +18,18 @@ public class Media {
          } else if (media < 5.0) {
              System.out.printf("Aluno reprovado");
          } else {
-             System.out.printf("Aluno em exame");
+             System.out.printf("Aluno em exame\n");
+             System.out.println("Digite sua nota do exame:\t");
+             exame = scanner.nextFloat();
+             
+             fim = (media + exame) / 2;
+             System.out.printf("Média final: %.1f\t", fim);
+             
+             if (fim >= 5.0) {
+                 System.out.printf("Aluno aprovado");
+             } else {
+                 System.out.printf("Aluno reprovado");
+             }
          }
-         
-         // exame
     }
 }
