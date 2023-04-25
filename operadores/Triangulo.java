3e0e3b0f641eb2eb49eda804392f8ea599982401
laura <105574779+laurasultan@users.noreply.github.com>
2023-04-25 16:53:57 -0300

    Triângulo INCOMPLETO

---
+++ operadores/Triangulo.java
@@ -0,0 +1,32 @@
+// --------------------------------- CÓDIGO INCOMPLETO --------------------------------- //
+// ------------------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------------------- //
+
+import java.util.Scanner;
+
+public class Triangulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float a, b, c;
+        System.out.println("Digite os valores de A, B e C:\t");
+        a = scanner.nextFloat();
+        b = scanner.nextFloat();
+        c = scanner.nextFloat();
+        
+        // (a >= b + c) ? 
+        
+        if (a >= b + c) {
+            System.out.printf("Não forma triângulo");
+        } else if (a * a = b * b + c * c) {
+            System.out.printf("Triângulo retângulo");
+        } else if (a * a > b * b + c * c) {
+            System.out.printf("Triângulo obstusângulo");
+        } else if (a * a < b * b + c * c) {
+            System.out.printf("Triângulo acutângulo");
+        } else if (a = b = c) {
+            System.out.printf("Triângulo equilátero");
+        } else if (a = b || a = c || b = c) {
+            System.out.printf("Triângulo isósceles");
+        }
+    }
+}
